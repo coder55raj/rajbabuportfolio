73eaf533fcff06d241dcda36092a457b816bc26c
coder55raj <110777848+coder55raj@users.noreply.github.com>
2022-08-07 11:12:15 -0700

    rajbabu

---
+++ 2021021124_tictoe.c
@@ -0,0 +1,109 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>portfollio</title>
+    <link rel="stylesheet" href="style.css" class="rel">
+</head>
+
+<body class="light">
+
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active ">
+                    <a class="nav-link .text-light point" href="./home.html">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item active point">
+                    <a class="nav-link" href="./about.html">About</a>
+                </li>
+                <li class="nav-item dropdown active point">
+                   
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="./webdesigning.html">Web Designing</a>
+                      
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active point" href="./contacus.html">Contact Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active point" href="./cv.html">Cv</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active point" href="./services.html">Services</a>
+                </li>
+            </ul>
+            
+        </div>
+    </nav>
+    <div class="container my-4  fbout">
+      <h3>About Me</h3>
+      <P>Hello,I am Harsh keshari currently I am Pursuing B.TECH(Computer Science And Engineering) from Madan Mohan Malavivya
+        University of Technology,Gorakhpur.I have passed my Intermidiate Examination in 2020.I am begineer coder,
+        begineer Web Designer and editor .I love to explore diffrent new technology.
+      </P>
+      
+    </div>
+    <div class="container">
+      <h3>Skills</h3>
+
+      <h5 class="my-4">Html</h5>
+      <div class="progress ">
+        <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" style="width: 95%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+      </div>
+      <h5 class="my-4">Node JS</h5>
+      <div class="progress ">
+        <div class="progress-bar progress-bar-striped bg-success progress-bar-animated" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+      </div>
+      <h5 class="my-4">Java Script</h5>
+      <div class="progress ">
+        <div class="progress-bar progress-bar-striped bg-info progress-bar-animated" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+      </div>
+      <h5 class="my-4">CSS</h5>
+      <div class="progress ">
+        <div class="progress-bar progress-bar-striped bg-warning progress-bar-animated" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+      </div>
+      <h5 class="my-3">C language</h5>
+      <div class="progress ">
+        <div class="progress-bar progress-bar-striped bg-danger progress-bar-animated" role="progressbar" style="width: 90%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+      </div>
+    </div>
+    
+      
+    
+   
+
+
+
+
+</div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
